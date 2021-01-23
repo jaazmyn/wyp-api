35b6551c84d4ba8564271bdce713c7ecfc9bebfe
Jasmin Jenull <jasmin.jenull@gmx.at>
2021-01-23 18:20:02 +0100

    Add tests for WateringEvent model

--- spec/models/watering_event_spec.rb
+++ spec/models/watering_event_spec.rb
@@ -3,5 +3,24 @@
 require 'rails_helper'
 
 RSpec.describe WateringEvent, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "has a date" do
+    user = User.create!(name: "test", email: "test@test.com", password: "123456")
+    plant = Plant.create!(name: "Monstera", user: user)
+    watering_event = WateringEvent.new(date: Date.today - 10, plant: plant)
+    expect(watering_event.date).to eq(Date.today - 10)
+  end
+
+  it "has a note" do
+    user = User.create!(name: "test", email: "test@test.com", password: "123456")
+    plant = Plant.create!(name: "Monstera", user: user)
+    watering_event = WateringEvent.new(date: Date.today - 10, note: "one glass of water", plant: plant)
+    expect(watering_event.note).to eq("one glass of water")
+  end
+
+  it "belongs to a plant" do
+    user = User.create!(name: "test", email: "test@test.com", password: "123456")
+    plant = Plant.create!(name: "Monstera", user: user, watering_interval: 10)
+    watering_event = WateringEvent.create!(plant: plant, date: Date.today)
+    expect(watering_event.plant).to eq(plant)
+  end
 end
