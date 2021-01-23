d387033e3c5488caf43c5447a56136c940975fd1
Jasmin Jenull <jasmin.jenull@gmx.at>
2021-01-23 17:06:51 +0100

    Add config to generate rspec tests

--- config/application.rb
+++ config/application.rb
@@ -21,8 +21,13 @@ Bundler.require(*Rails.groups)
 
 module WypApi
   class Application < Rails::Application
+    config.generators do |generate|
+      generate.test_framework :rspec, fixture: false
+    end
+
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
+
 
     # Configuration for the application, engines, and railties goes here.
     #
