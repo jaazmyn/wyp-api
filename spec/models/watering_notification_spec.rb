80489678b3b95539df762d09f05fb0d8129becc2
Jasmin Jenull <jasmin.jenull@gmx.at>
2021-01-23 18:23:10 +0100

    Add tests for WateringNotification model

--- spec/models/watering_notification_spec.rb
+++ spec/models/watering_notification_spec.rb
@@ -3,5 +3,17 @@
 require 'rails_helper'
 
 RSpec.describe WateringNotification, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "has a date" do
+    user = User.create!(name: "test", email: "test@test.com", password: "123456")
+    plant = Plant.create!(name: "Monstera", user: user)
+    watering_notification = WateringNotification.new(date: Date.today - 10)
+    expect(watering_notification.date).to eq(Date.today - 10)
+  end
+
+  it "belongs to a plant" do
+    user = User.create!(email: "test@test.com", password: "123456")
+    plant = Plant.create!(name: "Monstera", user: user, watering_interval: 10)
+    watering_notification = WateringNotification.create!(plant: plant, date: Date.today)
+    expect(watering_notification.plant).to eq(plant)
+  end
 end
