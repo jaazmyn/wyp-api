5a2ab3e8a8731722a527609ce360d89c2ce18660
Jasmin Jenull <jasmin.jenull@gmx.at>
2021-01-23 18:39:15 +0100

    Add seed file

--- Gemfile
+++ Gemfile
@@ -24,6 +24,9 @@ gem 'devise'
 # Reduces boot times through caching; required in config/boot.rb
 gem 'bootsnap', '>= 1.4.4', require: false
 
+gem 'faker', :git => 'https://github.com/faker-ruby/faker.git', :branch => 'master'
+
+gem 'httparty'
 # Use Rack CORS for handling Cross-Origin Resource Sharing (CORS), making cross-origin AJAX possible
 # gem 'rack-cors'
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,65 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'httparty'
+require "faker"
+
+puts "Destroying users.."
+User.destroy_all
+
+puts "Destroying plants..."
+Plant.destroy_all
+
+puts "Destroying watering events.."
+WateringEvent.destroy_all
+
+puts "Destroying fertilizing events events.."
+FertilizingEvent.destroy_all
+
+puts "Destroying water notifications..."
+WaterNotification.destroy_all
+
+puts "Destroying fertilize notifications..."
+FertilizerNotification.destroy_all
+
+user = User.create!(name: "jasmin", email: "jasmin@test.com", password: "123456")
+
+response = HTTParty.get(
+  'https://trefle.io/api/v1/plants', format: :plain,
+  query: {
+    "token": ENV['TREFLE_KEY']
+  }
+)
+
+plants = JSON.parse(response, symbolize_names: true)
+
+plants[:data].each do |plant_info|
+
+    plant = Plant.create!(
+      common_name: plant_info[:common_name],
+      scientific_name: plant_info[:scientific_name],
+      nickname: Faker::Name.unique.name,
+      watering_interval: 7,
+      fertilizing_interval: 7,
+      user: user
+    )
+
+  1.times do
+    watering_event = WateringEvent.new(
+      date: Date.today,
+      plant: plant,
+      note: "One glass of water"
+      )
+    watering_event.save!
+  end
+
+  1.times do
+    fertilizing_event = FertilizingEvent.new(
+      date: Date.today,
+      plant: plant
+      )
+    fertilizing_event.save!
+  end
+
+  puts "Saving #{plant.common_name}..."
+end
