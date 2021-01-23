cc3481124dbafabc2d18d1249112b65dd8419ede
Jasmin Jenull <jasmin.jenull@gmx.at>
2021-01-23 18:24:30 +0100

    Add tests for FertilizingNotification model

--- spec/models/fertilizing_notification_spec.rb
+++ spec/models/fertilizing_notification_spec.rb
@@ -3,5 +3,17 @@
 require 'rails_helper'
 
 RSpec.describe FertilizingNotification, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "has a date" do
+    user = User.create!(name: "test", email: "test@test.com", password: "123456")
+    plant = Plant.create!(name: "Monstera", user: user)
+    fertilizing_notification = FertilizingNotification.new(date: Date.today - 10)
+    expect(fertilizing_notification.date).to eq(Date.today - 10)
+  end
+
+  it "belongs to a plant" do
+    user = User.create!(email: "test@test.com", password: "123456")
+    plant = Plant.create!(name: "Monstera", user: user, watering_interval: 10)
+    fertilizing_notification = FertilizingNotification.create!(plant: plant, date: Date.today)
+    expect(fertilizing_notification.plant).to eq(plant)
+  end
 end
